6a26bbe7442527358cc3c0e17742e9bd7ae7b7ac
ShariqNaeem <muhammad.shariq64@gmail.com>
2023-12-25 21:56:16 +0500

    update createPlaylist test for making it dynamic

--- src/test/java/com/spotify/oauth2/tests/PlayListTests.java
+++ src/test/java/com/spotify/oauth2/tests/PlayListTests.java
@@ -1,14 +1,17 @@
 package com.spotify.oauth2.tests;
 
 import com.spotify.oauth2.apis.RestResource;
+import com.spotify.oauth2.apis.StatusCode;
 import com.spotify.oauth2.pojos.Playlist;
 import com.spotify.oauth2.utils.ConfigLoader;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import static com.spotify.oauth2.apis.Route.PLAYLISTS;
+import static com.spotify.oauth2.apis.Route.USERS;
 import static com.spotify.oauth2.utils.FakerUtils.generateDescription;
 import static com.spotify.oauth2.utils.FakerUtils.generateName;
-import static com.spotify.oauth2.utils.TokenManager.getToken;
+import static com.spotify.oauth2.apis.TokenManager.getToken;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -19,14 +22,13 @@ public class PlayListTests {
         // FakerApi Used for fake data
         Playlist playlist = playlistBuilder(generateName(), generateDescription(), false);
 
-        Response response = RestResource.post("/v1/users/"+ ConfigLoader.getConfigLoaderInstance().getPropertyValue("user_id") + "/playlists", getToken(), playlist);
-        assertThat(response.statusCode(), is(equalTo(201)));
+        Response response = RestResource.post(USERS +"/"+ ConfigLoader.getConfigLoaderInstance().getPropertyValue("user_id") + PLAYLISTS, getToken(), playlist);
+        assertThat(response.statusCode(), equalTo(StatusCode.CODE_201.getCode()));
 
         Playlist deserializePlaylist = response.as(Playlist.class);
         assertThat(deserializePlaylist.getName(), equalTo(playlist.getName()));
         assertThat(deserializePlaylist.getDescription(), equalTo(playlist.getDescription()));
         assertThat(deserializePlaylist.get_public(), equalTo(playlist.get_public()));
-
     }
 
     public Playlist playlistBuilder(String name, String description, boolean _public) {
