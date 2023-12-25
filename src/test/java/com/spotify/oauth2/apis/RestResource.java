5ffd04e5df5f6507175ea185f15c18484c8fb039
ShariqNaeem <muhammad.shariq64@gmail.com>
2023-12-25 21:54:43 +0500

    change functions for the Request and Response Specification

--- src/test/java/com/spotify/oauth2/apis/RestResource.java
+++ src/test/java/com/spotify/oauth2/apis/RestResource.java
@@ -6,30 +6,31 @@ import org.testng.annotations.Test;
 
 import java.util.HashMap;
 
+import static com.spotify.oauth2.apis.Route.API;
+import static com.spotify.oauth2.apis.Route.TOKEN;
+import static com.spotify.oauth2.apis.SpecBuilders.*;
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 
 public class RestResource {
-    public static Response postAccount(HashMap<String, String> formParams){
-        return given().
-                baseUri("https://accounts.spotify.com").
-                contentType(ContentType.URLENC).
-                formParams(formParams).log().all().
-        when().
-                post("/api/token").
-        then().log().all().
-                extract().response();
+    public static Response postAccount(HashMap<String, String> formParams) {
+        return given()
+                    .spec(getAccountRequestSpec())
+                    .formParams(formParams)
+                .when()
+                    .post(API + TOKEN)
+                .then().spec(getResponseSpec())
+                    .extract().response();
     }
 
-    public static Response post(String path, String token, Object body){
-        return given().
-                    baseUri("https://api.spotify.com").
-                    auth().oauth2(token).
-                    body(body).
-                    log().all().
-                when().
-                    post(path).
-                then().log().all().
-                    extract().response();
+    public static Response post(String path, String token, Object body) {
+        return given().spec(getRequestSpec())
+                    .body(body)
+                    .auth().oauth2(token) //this way can be also used for token
+                    //.header("Authorization", "Bearer " + token)
+                .when()
+                    .post(path)
+                .then().spec(getResponseSpec())
+                    .extract().response();
     }
 }
